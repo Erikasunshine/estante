352ac37179f1f7effa67b55fbb7092bccab8e08f
LuanaLucia <luanasantos@outlook.com>
2023-05-06 02:24:09 -0300

    firts commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import CardAvaliacao from './Components/CardAvaliacao/CardAvaliacao';
+import Estante from './Components/Estante/Estante';
+import Header from './Components/Header/Header';
+import Menu from './Components/Menu/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <Header />
+
+      <hr />
+
+      <h2>Minha Estante</h2>
+
+      <div className='principal'>
+        <Menu />
+        <Estante />
+      </div>
+      <h2>Ultimas Avaliacoes</h2>
+      <div className='principal'>
+        <CardAvaliacao titulo="Titulo da Resenha" resenha="Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
+
+        <CardAvaliacao titulo="Titulo da Resenha" resenha="Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
+      </div>
     </div>
   );
 }
