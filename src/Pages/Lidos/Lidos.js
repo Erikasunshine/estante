4b7815efc9a5379cd13442ac649b4853e7a90dc4
Erikasunshine <erikaandradecruz@gmail.com>
2023-05-08 01:42:40 -0300

    Estante1

---
+++ src/Pages/Lidos/Lidos.js
@@ -0,0 +1,30 @@
+import styles from "./Lidos.css";
+function Lidos() {
+    return (
+        <section className={styles.Lidos}>
+        <h1>Lidos</h1>
+        </section>
+    );
+
+    <div className="Lidos">
+
+      <Header />
+
+      <hr />
+
+      <h2>Minha Estante</h2>
+
+      <div className='principal'>
+        <Menu />
+        <Estante />
+      </div>
+      <h2>Ultimas Avaliacoes</h2>
+      <div className='principal'>
+        <CardAvaliacao titulo="Titulo da Resenha" resenha="Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
+
+        <CardAvaliacao titulo="Titulo da Resenha" resenha="Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like)." />
+      </div>
+    </div>
+}
+
+export default Lidos;
